21e1f59a2134ff5b6e047e82dc768f1e6b595b4d
satyavamsi <satyavamsi222.21@gmail.com>
2019-06-23 17:04:06 +0530

    added sample cards

--- lib/main.dart
+++ lib/main.dart
@@ -9,103 +9,280 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Scaffold(
+          appBar: PreferredSize(
+            preferredSize: Size.fromHeight(80),
+            child: AppBar(
+              title: Container(
+                transform: Matrix4.translationValues(-10.0, 10.0, 0.0),
+                child: Row(children: <Widget>[
+                  Center(
+                    child: ImageWidget(),
+                  ),
+                  Container(
+                    width: 36,
+                    height: 36,
+                    margin: EdgeInsets.only(right: 20, left: 10),
+                    decoration: new BoxDecoration(
+                      color: Color.fromRGBO(0, 0, 60, 1),
+                      borderRadius: new BorderRadius.circular(25.0),
+                      border: new Border.all(
+                        width: 0.0,
+                        color: Colors.black,
+                      ),
+                    ),
+                    child: IconButton(
+                      color: Colors.black,
+                      icon: Icon(
+                        Icons.location_on,
+                        size: 26,
+                        color: Color.fromRGBO(255, 255, 255, 1),
+                      ),
+                      tooltip: 'save',
+                    ),
+                  ),
+                  Container(
+                    width: 36,
+                    height: 36,
+                    decoration: new BoxDecoration(
+                      color: Color.fromRGBO(0, 0, 60, 1),
+                      borderRadius: new BorderRadius.circular(25.0),
+                      border: new Border.all(
+                        width: 0.0,
+                        color: Colors.black,
+                      ),
+                    ),
+                    child: IconButton(
+                      icon: Icon(
+                        Icons.search,
+                        size: 26,
+                        color: Color.fromRGBO(255, 255, 255, 1),
+                      ),
+                      tooltip: 'save',
+                    ),
+                  ),
+                ]),
+              ),
+              elevation: 0.0,
+              // leading: Container(
+              //   alignment: Alignment.center,
+              //   child: Column(
+              //     mainAxisAlignment: MainAxisAlignment.start,
+              //     crossAxisAlignment: CrossAxisAlignment.center,
+              //     children: <Widget>[
+              //       Center(
+              //         child: ImageWidget(),
+              //       ),
+              //     ],
+              //   ),
+              // ),
+              actions: <Widget>[
+                new Container(
+                  transform: Matrix4.translationValues(0.0, 10.0, 0.0),
+                  margin: EdgeInsets.only(top: 5, right: 20, bottom: 10),
+                  padding: EdgeInsets.only(left: 0, right: 20,),
+                  decoration: new BoxDecoration(
+                    color: Colors.tealAccent[400],
+                    borderRadius: new BorderRadius.circular(25.0),
+                    border: new Border.all(
+                      width: 0.0,
+                      color: Colors.black,
+                    ),
+                  ),
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        IconButton(
+                          icon: Icon(
+                            Icons.add_box,
+                            size: 26,
+                            color: Color.fromRGBO(255, 255, 255, 1),
+                          ),
+                          tooltip: 'add',
+                        ),
+                        Text("POST", style: TextStyle(fontSize: 12,fontWeight: FontWeight.bold ),
+                        ),
+                      ]),
+                )
+              ],
+              backgroundColor: Color.fromRGBO(25, 0, 127, 0.9),
+            ),
+          ),
+          body: Container(
+            color: Color.fromRGBO(25, 0, 127, 0.9),
+            child: ListView(
+              children: <Widget>[
+                PostCard(),
+                PostCard(),
+                PostCard(),
+                PostCard(),
+                PostCard(),
+                PostCard(),
+                PostCard(),
+                PostCard(),
+                PostCard(),
+                PostCard(),
+              ],
+            ),
+          )),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class Header extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+    return Center(
+      child: Text('Hello'),
+    );
+  }
+}
+
+class PostCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Card(
+        color: Colors.white,
+        elevation: 20,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(25.0),
+        ),
+        margin: EdgeInsets.fromLTRB(10, 15, 10, 15),
+        child: Padding(
+          padding: EdgeInsets.all(0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Container(
+                transform: Matrix4.translationValues(0.0, 10.0, 0.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    Padding(
+                        padding: EdgeInsets.all(0),
+                        child: IconButton(
+                          iconSize: 24,
+                          icon: Icon(Icons.more_vert),
+                          tooltip: 'Increase volume by 10',
+                        )),
+                  ],
+                ),
+              ),
+              Row(
+                children: <Widget>[
+                  ImageWidget(),
+                  Text(
+                    'Margot',
+                    style: TextStyle(
+                        color: Color.fromRGBO(25, 0, 127, 0.8), fontSize: 14),
+                  ),
+                  Text(
+                    ' - 1 day ago',
+                    style: TextStyle(
+                        color: Color.fromRGBO(25, 0, 127, 0.8), fontSize: 14),
+                  )
+                ],
+              ),
+              Padding(
+                  padding: EdgeInsets.only(left: 42, bottom: 20),
+                  child: Text(
+                    'Any dasara celebrations nearby',
+                    style: TextStyle(
+                        fontSize: 28, color: Color.fromRGBO(25, 0, 127, 0.8)),
+                  )),
+              Padding(
+                padding: EdgeInsets.fromLTRB(24, 0, 24, 0),
+                child: Divider(
+                  color: Colors.grey,
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(30, 0, 30, 0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Row(
+                      children: <Widget>[
+                        IconButton(
+                          icon: Icon(
+                            Icons.star,
+                            color: Color.fromRGBO(25, 0, 127, 0.5),
+                          ),
+                          tooltip: 'save',
+                        ),
+                        Text(
+                          'Save',
+                          style: TextStyle(
+                            color: Color.fromRGBO(25, 0, 127, 0.5),
+                          ),
+                        )
+                      ],
+                    ),
+                    Row(
+                      children: <Widget>[
+                        IconButton(
+                          icon: Icon(
+                            Icons.thumb_up,
+                            color: Color.fromRGBO(25, 0, 127, 0.5),
+                          ),
+                          tooltip: 'Like',
+                        ),
+                        Text(
+                          'Like',
+                          style: TextStyle(
+                            color: Color.fromRGBO(25, 0, 127, 0.5),
+                          ),
+                        )
+                      ],
+                    ),
+                    Row(
+                      children: <Widget>[
+                        IconButton(
+                          icon: Icon(
+                            Icons.mode_comment,
+                            color: Colors.deepOrange,
+                          ),
+                          tooltip: 'answer',
+                        ),
+                        Text(
+                          'Answer',
+                          style: TextStyle(color: Colors.deepOrange),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+class ImageWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // ...
+    // AssetImage assetImage = ;
+    // Image image = Image(image: assetImage);
+    return Container(
+      width: 36.0,
+      height: 36.0,
+      margin: EdgeInsets.all(10),
+      decoration: new BoxDecoration(
+        shape: BoxShape.circle,
+        image: new DecorationImage(
+          fit: BoxFit.fill,
+          image: AssetImage('images/margot.jpg'),
+        ),
+      ),
+    );
+    // ...
   }
 }
